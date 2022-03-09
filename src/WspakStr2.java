125920648a1020ba23ab6854a5a7efa5a217539c
LachTomasz <lach.tomasz@gmail.com>
2022-03-09 22:23:11 +0100

    small changed

--- src/WspakStr2.java
+++ src/WspakStr2.java
@@ -1,2 +1,13 @@
 public class WspakStr2 {
+    public static void main(String[] args) {
+        String text = "programowanie";
+        System.out.println("zawartość Stringa text:\t"+text);
+        System.out.println();
+        String textWspak = "";
+        for (int i = text.length()-1; i >= 0 ; i--) {
+            textWspak = textWspak + text.charAt(i);
+        }
+        text = textWspak;
+        System.out.println("po trans:\t"+text);
+    }
 }
