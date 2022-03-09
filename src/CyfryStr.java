cd34076b01fa68f3edcffd111d7a88d0f5d42493
LachTomasz <lach.tomasz@gmail.com>
2022-03-09 22:23:11 +0100

    small changed

---
+++ src/CyfryStr.java
@@ -0,0 +1,8 @@
+public class CyfryStr {
+    public static void main(String[] args) {
+        String cyfry = "";
+        for (int i = 48; i <58; i++){
+            cyfry = cyfry + i;
+        }
+    }
+}
