a520318481b82d07e550215058a2de536fda4ff7
LachTomasz <lach.tomasz@gmail.com>
2022-03-09 21:48:48 +0100

    Initial commit

---
+++ src/WspakjStr1.java
@@ -0,0 +1,11 @@
+public class WspakjStr1 {
+    public static void main(String[] args) {
+        String text = "programowanie";
+        System.out.println("zawartość Stringa text:\t"+text);
+        System.out.println();
+        System.out.println("A teraz wspak:");
+        for (int i = text.length()-1; i >= 0 ; i--) {
+            System.out.print(text.charAt(i));
+        }
+    }
+}
