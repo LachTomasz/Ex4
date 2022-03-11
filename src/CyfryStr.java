076d09d6dc45281cd293741963bd49db594c8c1c
LachTomasz <lach.tomasz@gmail.com>
2022-03-12 00:09:19 +0100

    small changed

--- src/CyfryStr.java
+++ src/CyfryStr.java
@@ -3,7 +3,7 @@ public class CyfryStr {
     public static void main(String[] args) {
         String cyfry = "";
         for (int i = 48; i <58; i++){
-            cyfry = cyfry + (char)i;;
+            cyfry += (char)i;;
         }
         System.out.println(cyfry);
     }
