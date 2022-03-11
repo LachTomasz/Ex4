da5f5988e339550038dd7f19e0ad5c3fd37ac7f7
LachTomasz <lach.tomasz@gmail.com>
2022-03-12 00:17:56 +0100

    small changed

--- src/CyfryStrHex.java
+++ src/CyfryStrHex.java
@@ -1,11 +1,11 @@
 public class CyfryStrHex {
     public static void main(String[] args) {
-        String Hex = "";
+        StringBuilder Hex = new StringBuilder();
         for (int i = 48; i < 58; i++){
-            Hex += (char)i;
+            Hex.append((char) i);
         }
         for (int i = 65; i < 71; i++){
-            Hex += (char)i;
+            Hex.append((char) i);
         }
         System.out.println(Hex);
 
