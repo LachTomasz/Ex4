8e33e1860ffbd9a383ca966f5c0683fedfeafe04
LachTomasz <lach.tomasz@gmail.com>
2022-03-12 00:17:56 +0100

    small changed

--- src/CyfryStrHex.java
+++ src/CyfryStrHex.java
@@ -1,13 +1,24 @@
+import java.lang.Character;
 public class CyfryStrHex {
     public static void main(String[] args) {
-        String Hex = "";
-        for (int i = 48; i < 58; i++){
-            Hex += (char)i;
-        }
-        for (int i = 65; i < 71; i++){
-            Hex += (char)i;
-        }
+
+        StringBuilder Hex = new StringBuilder();
+
+        for (int i = 48; i < 58; i++)
+            Hex.append((char) i);
+
+        for (int i = 65; i < 71; i++)
+            Hex.append((char) i);
+
         System.out.println(Hex);
 
+        //ewentualnie
+        StringBuilder Szesnastkowe = new StringBuilder();
+
+        for (int i = 0; i < 16; i++)
+            Szesnastkowe.append(Character.forDigit(i, 16));
+
+        Szesnastkowe = new StringBuilder(Szesnastkowe.toString().toUpperCase());
+        System.out.println(Szesnastkowe);
     }
 }
